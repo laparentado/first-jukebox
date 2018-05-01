2be0985cebf4d063a9e42cf2bbec4bf153002149
Lori Aparentado <loriaparentado@lori-aparentados-macbook.local>
2018-05-01 10:03:19 -0400

    add audio and play functions

--- index.js
+++ index.js
@@ -4,39 +4,44 @@ var stop = document.getElementById("stop")
 var ff = document.getElementById("ff")
 var song = document.getElementById("song")
 
+var catalog = ["be my baby - the ronettes", "can't hurry love - the supremes", "why do fools fall in love - frankie lymon"]
+
+var mySongs = new Audio([""]);
+
 class Jukebox{
-  constructor(songName){
-    this.songName = songName;
-  }
+
   playing(){
-    song.innerHTML = "this song is playing"
+    for(let i=0; i<mySongs.length;i++){
+      song.innerHTML = mySongs[i].play()
+    }
   }
   pausing(){
-
+    song.innerHTML = "song paused"
   }
   stopping(){
-
+    song.innerHTML = "song stopped"
   }
   forwarding(){
-
+    for(let i=0;i<mySongs.length; i++){
+    song.innerHTML = mySongs[i-1]
   }
+}
 }
 
 var myJuke = new Jukebox()
 
-play.addEventListener("click" function(){
+play.addEventListener("click", function(){
   myJuke.playing()
-
 })
 
-pause.addEventListener("click" function(){
-
+pause.addEventListener("click", function(){
+  myJuke.pausing()
 })
 
-stop.addEventListener("click" function(){
-
+stop.addEventListener("click", function(){
+  myJuke.stopping()
 })
 
-ff.addEventListener("click" function(){
-
+ff.addEventListener("click", function(){
+  myJuke.forwarding()
 })
