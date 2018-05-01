2dd0d15de56de39e8c05af7bf1b843a7c928137e
Lori Aparentado <loriaparentado@lori-aparentados-macbook.local>
2018-05-01 16:15:04 -0400

    song names work

--- index.js
+++ index.js
@@ -7,46 +7,55 @@ var song = document.getElementById("song")
 var audio = document.createElement("audio")
 document.body.appendChild(audio)
 
-var currentSong = 0;
 
-var song1 = new Audio("dig.mp3")
-var song2 = new Audio("prick.mp3")
-var song3 = new Audio("burn.mp3")
-var song4 = new Audio("pressure.mp3")
-var song5 = new Audio("read.mp3")
 
-function Song(title, artist, url){
+function Song(title, artist, file){
   this.title = title;
   this.artist = artist;
-  this.url = url;
+  this.file = file;
 }
 
-song1 = new Song()
+var song1 = new Song("dig", "bacchae", "dig.mp3")
+var song2 = new Song("prick","bacchae","prick.mp3")
+var song3 = new Song("burn","bacchae","burn.mp3")
+var song4 = new Song("pressure","bacchae","pressure.mp3")
+var song5 = new Song("read","bacchae","read.mp3")
 
+var currentSong = 0;
+var myAudio = new Audio();
 
 class Jukebox{
   constructor(){
     this.catalog = [song1, song2, song3, song4, song5]
   }
   playing(){
-      this.catalog[currentSong].play()
-      song.innerHTML = currentSong
+      myAudio.src = this.catalog[currentSong].file
+      myAudio.play()
+      song.innerHTML = this.catalog[currentSong].title +"-"+ this.catalog[currentSong].artist
   }
   pausing(){
-    this.catalog[currentSong].pause()
+    myAudio.src = this.catalog[currentSong].file
+    myAudio.pause()
+    song.innerHTML = "paused"
   }
   stopping(){
-    this.catalog[currentSong].pause()
-    this.catalog[currentSong].currentTime =0;
+    myAudio.src = this.catalog[currentSong].file
+    myAudio.pause()
+    myAudio.src = this.catalog[currentSong].file.currentTime =0;
   }
   forwarding(){
-    this.catalog[currentSong].pause()
-    if(currentSong == this.catalog.length-1){
+    myAudio.src = this.catalog[currentSong].file
+    myAudio.pause()
+    if(myAudio.src = this.catalog[currentSong].file == this.catalog.length-1){
       currentSong =0;
-    this.catalog[currentSong].play()
+    myAudio.src = this.catalog[currentSong].file
+    myAudio.play()
+    song.innerHTML = this.catalog[currentSong].title +"-"+ this.catalog[currentSong].artist
   }else{
     currentSong++
-    this.catalog[currentSong].play()
+    myAudio.src = this.catalog[currentSong].file
+    myAudio.play()
+    song.innerHTML = this.catalog[currentSong].title +"-"+ this.catalog[currentSong].artist
   }
   }
 }
